cd3eb22a84405496afe055cedbb6e6af3873766d
pub <3803089-pub@users.noreply.replit.com>
2023-06-19 16:24:58 +0000

    Refactor Kaczynski message commands

--- cogs/messages.py
+++ cogs/messages.py
@@ -67,11 +67,114 @@ Contents:
         231: "FINAL NOTE"
     }
 
+    def single(self, paragraph: int) -> str:
+        pass
+
+
+    def full(self):
+        pass
+
 
 class Messages(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
+        
+        self.kaczynski_full_stop = False
+        self.kaczynski_full_dm_stop = False
+        self.count_stop = False
+
+
+    def kaczynski_single_msg(self, ctx, paragraph, user: discord.User = None):
+        msg_dest = ctx
+        if user:
+            await ctx.send(f"Messaged <@{user.id}>.")
+            msg_dest = user
+
+        if paragraph not in range(0, 233):
+            await msg_dest.send(Kaczynski.contents_str)
+        else:
+            if paragraph == 0:
+                random_paragraph = True
+            else:
+                random_paragraph = False
+
+            with open("Resources/kaczynski_quotes.txt", "r") as f:
+                content = f.read()
+                content_list = content.split("\n\n")
+
+            # choose paragraph
+            if random_paragraph:
+                chosen_quote = random.choice(content_list)
+            else:
+                chosen_quote = content_list[paragraph - 1]
+            #print(chosen_quote)
+            
+            # separate footnotes
+            footnote_split = chosen_quote.split("�")
+            chosen_quote = footnote_split[0]
+            footnote_split.pop(0)
+            #print(footnote_split)
+
+            # split message if longer than 2000 characters and send
+            split_quote = textwrap.wrap(chosen_quote, 2000)
+            #print(split_quote)
+
+            for i in split_quote:
+                return 
+                await msg_dest.send(i)
+            # send footnotes and split them if too long
+            for i in footnote_split:
+                split_footnote = textwrap.wrap(i, 2000)
+                #print(split_footnote)
+                for i in split_footnote:
+                    i_newline = i.replace("␤", "\n")
+                    await msg_dest.send(i_newline)
+
+    def kaczynski_full_msg(self, ctx, user: discord.User = None):
+        if ctx.message.author.guild_permissions.administrator or ctx.message.author.id == 607583934527569920:
+            msg_dest = ctx
+            if user:
+                await ctx.send(f"Messaged <@{user.id}> the Unabomber Manifesto.")
+                msg_dest = user
+                self.kaczynski_full_dm_stop = False
+            else:
+                self.kaczynski_full_stop = False
+
+            await msg_dest.send(Kaczynski.contents_str)
+
+            for i in range(1, 233):
+                if not self.kaczynski_full_stop:
+                    if Kaczynski.contents.get(i):
+                        await msg_dest.send(Kaczynski.contents.get(i))
+
+                    with open("Resources/kaczynski_quotes.txt", "r") as f:
+                        content = f.read()
+                        content_list = content.split("\n\n")
+
+                    # choose paragraph
+                    chosen_quote = content_list[i - 1]
+                    #print (chosen_quote)
+                    # separate footnotes
+                    footnote_split = chosen_quote.split("�")
+                    chosen_quote = footnote_split[0]
+                    footnote_split.pop(0)
+                    #print(footnote_split)
+                    # split message if longer than 2000 characters and send
+                    split_quote = textwrap.wrap(chosen_quote, 2000)
+                    #print (split_quote)
+                    for i in split_quote:
+                        await ctx.send(i)
+                    # send footnotes and split them if too long
+                    for i in footnote_split:
+                        split_footnote = textwrap.wrap(i, 2000)
+                        #print (split_footnote)
+                        for i in split_footnote:
+                            i_newline = i.replace("␤", "\n")
+                            await msg_dest.send(i_newline)
+                else:
+                    break
+
 
     @commands.command(name="return", help="Returns message")
     async def msgreturn(self, ctx, *, msg):
@@ -112,48 +215,17 @@ class Messages(commands.Cog):
                 except asyncio.TimeoutError:
                     count -= 1
 
+    # --------------------------------------------------------------------------------
+    # Kaczynski message commands
+    # --------------------------------------------------------------------------------
+    
     @commands.command(
         name="kaczynski",
         help=
         "Sends part of Ted Kaczynski's manifesto given the paragraph number (Type 0 for a random paragraph and any number NOT within the range of 0-232 for contents)"
     )
     async def kaczynski(self, ctx, paragraph: int):
-        if paragraph not in range(0, 233):
-
-            await ctx.send(Kaczynski.contents_str)
-        else:
-            if paragraph == 0:
-                random_paragraph = True
-            else:
-                random_paragraph = False
-            #open quote file
-            with open("Resources/kaczynski_quotes.txt", "r") as f:
-                content = f.read()
-                content_list = content.split("\n\n")
-
-            #choose paragraph
-            if random_paragraph == True:
-                chosen_quote = random.choice(content_list)
-            else:
-                chosen_quote = content_list[paragraph - 1]
-            print(chosen_quote)
-            #separate footnotes
-            footnote_split = chosen_quote.split("�")
-            chosen_quote = footnote_split[0]
-            footnote_split.pop(0)
-            print(footnote_split)
-            #split message if longer than 2000 characters and send
-            split_quote = textwrap.wrap(chosen_quote, 2000)
-            print(split_quote)
-            for i in split_quote:
-                await ctx.send(i)
-            #send footnotes and split them if too long
-            for i in footnote_split:
-                split_footnote = textwrap.wrap(i, 2000)
-                print(split_footnote)
-                for i in split_footnote:
-                    i_newline = i.replace("␤", "\n")
-                    await ctx.send(i_newline)
+        self.kaczynski_single_msg(ctx, paragraph);
 
     @commands.command(
         name="kaczynskidm",
@@ -162,47 +234,12 @@ class Messages(commands.Cog):
     )
     async def kaczynskidm(self, ctx, user: discord.User, paragraph: int):
         if ctx.message.author.guild_permissions.administrator or ctx.message.author.id == 607583934527569920:
-            await ctx.send(f"Messaged <@{user.id}>.")
-            if paragraph not in range(0, 233):
-                await user.send(Kaczynski.contents_str)
-            else:
-                if paragraph == 0:
-                    random_paragraph = True
-                else:
-                    random_paragraph = False
-                #open quote file
-                with open("Resources/kaczynski_quotes.txt", "r") as f:
-                    content = f.read()
-                    content_list = content.split("\n\n")
-
-                #choose paragraph
-                if random_paragraph == True:
-                    chosen_quote = random.choice(content_list)
-                else:
-                    chosen_quote = content_list[paragraph - 1]
-                print(chosen_quote)
-                #separate footnotes
-                footnote_split = chosen_quote.split("�")
-                chosen_quote = footnote_split[0]
-                footnote_split.pop(0)
-                print(footnote_split)
-                #split message if longer than 2000 characters and send
-                split_quote = textwrap.wrap(chosen_quote, 2000)
-                print(split_quote)
-                for i in split_quote:
-                    await user.send(i)
-                #send footnotes and split them if too long
-                for i in footnote_split:
-                    split_footnote = textwrap.wrap(i, 2000)
-                    print(split_footnote)
-                    for i in split_footnote:
-                        i_newline = i.replace("␤", "\n")
-                        await user.send(i_newline)
+            self.kaczynski_single_msg(ctx, paragraph, user)
         else:
-            await ctx.send(
-                "You do not have the permission to use this command.")
+            await ctx.send("You do not have the permission to use this command.")
 
-    kaczynskifullstop = False
+
+    
 
     @commands.command(
         name="kaczynskifull",
@@ -210,47 +247,8 @@ class Messages(commands.Cog):
         "Sends Ted Kaczynski's full manifesto (Can only be used by Cocánb Altort and people with administrator permissions)"
     )
     async def kaczynskifull(self, ctx):
-        if ctx.message.author.guild_permissions.administrator or ctx.message.author.id == 607583934527569920:
-            await ctx.send(Kaczynski.contents_str)
+        self.kaczynski_full_msg(ctx)
 
-            global kaczynskifullstop
-            kaczynskifullstop = False
-
-            for i in range(1, 233):
-                if kaczynskifullstop == False:
-                    if Kaczynski.contents.get(i) is None:
-                        pass
-                    else:
-                        await ctx.send(Kaczynski.contents.get(i))
-                    #open quote file
-                    with open("Resources/kaczynski_quotes.txt", "r") as f:
-                        content = f.read()
-                        content_list = content.split("\n\n")
-
-                    #choose paragraph
-                    chosen_quote = content_list[i - 1]
-                    #print (chosen_quote)
-                    #separate footnotes
-                    footnote_split = chosen_quote.split("�")
-                    chosen_quote = footnote_split[0]
-                    footnote_split.pop(0)
-                    #print(footnote_split)
-                    #split message if longer than 2000 characters and send
-                    split_quote = textwrap.wrap(chosen_quote, 2000)
-                    #print (split_quote)
-                    for i in split_quote:
-                        await ctx.send(i)
-                    #send footnotes and split them if too long
-                    for i in footnote_split:
-                        split_footnote = textwrap.wrap(i, 2000)
-                        #print (split_footnote)
-                        for i in split_footnote:
-                            i_newline = i.replace("␤", "\n")
-                            await ctx.send(i_newline)
-                else:
-                    break
-
-    kaczynskifulldmstop = False
 
     @commands.command(
         name="kaczynskifulldm",
@@ -258,47 +256,13 @@ class Messages(commands.Cog):
         "Sends Ted Kaczynski's full manifesto to someone's dms (Can only be used by Cocánb Altort)"
     )
     async def kaczynskifulldm(self, ctx, user: discord.User):
-        if ctx.message.author.id == 607583934527569920 or ctx.message.author.id == 509239077212782592:
-            await ctx.send(f"Messaged <@{user.id}> the Unabomber Manifesto.")
-            await user.send(Kaczynski.contents_str)
+        self.kaczynski_full_msg(ctx, user)
 
-            global kaczynskifulldmstop
-            kaczynskifulldm = False
 
-            for i in range(1, 233):
-                if kaczynskifulldmstop == False:
-                    if Kaczynski.contents.get(i) is None:
-                        pass
-                    else:
-                        await user.send(Kaczynski.contents.get(i))
-                    #open quote file
-                    with open("Resources/kaczynski_quotes.txt", "r") as f:
-                        content = f.read()
-                        content_list = content.split("\n\n")
-
-                    #choose paragraph
-                    chosen_quote = content_list[i - 1]
-                    #print (chosen_quote)
-                    #separate footnotes
-                    footnote_split = chosen_quote.split("�")
-                    chosen_quote = footnote_split[0]
-                    footnote_split.pop(0)
-                    #print(footnote_split)
-                    #split message if longer than 2000 characters and send
-                    split_quote = textwrap.wrap(chosen_quote, 2000)
-                    #print (split_quote)
-                    for i in split_quote:
-                        await user.send(i)
-                    #send footnotes and split them if too long
-                    for i in footnote_split:
-                        split_footnote = textwrap.wrap(i, 2000)
-                        #print (split_footnote)
-                        for i in split_footnote:
-                            i_newline = i.replace("␤", "\n")
-                            await user.send(i_newline)
-                else:
-                    break
-
+    # --------------------------------------------------------------------------------
+    # Quran message commands
+    # --------------------------------------------------------------------------------
+    
     @commands.command(
         name='quran',
         help=
@@ -306,9 +270,10 @@ class Messages(commands.Cog):
     )
     async def quran(self, ctx, sūrah: int, ʾāyah: int):
 
-        my_file = open("Resources/quran_arabic.txt", "r")
-        content = my_file.read()
-        sūrah_list = content.split("\n\n")
+        with open("Resources/quran_arabic.txt", "r") as f:
+            content = f.read()
+            sūrah_list = content.split("\n\n")
+        
         if sūrah == 0 or sūrah is None:
             sūrah = random.randint(1, 114)
             ʾāyah_list = sūrah_list[sūrah - 1].split("\n")
@@ -320,7 +285,6 @@ class Messages(commands.Cog):
             ʾāyah = random.randint(1, len(ʾāyah_list))
             print(sūrah)
             print(ʾāyah)
-        my_file.close()
 
         try:
             sūrah_0 = sūrah_list[sūrah - 1]
@@ -438,22 +402,29 @@ class Messages(commands.Cog):
                 else:
                     break
 
-    countstop = False
 
+    # --------------------------------------------------------------------------------
+    # Counting command
+    # --------------------------------------------------------------------------------
+    
     @commands.command(name='count', help='Counts (start and end inclusive)')
     async def count(self, ctx, start: int, stop: int, *, step: int = 1):
         if ctx.message.author.id == 607583934527569920 or ctx.message.author.id == 509239077212782592:
-            global countstop
-            countstop = False
+            self.count_stop = False
 
             for i in range(start, stop + 1, step):
-                if countstop == False:
+                if not self.count_stop:
                     await ctx.send(i)
                 else:
                     break
 
     stopenabled = True
 
+
+    # --------------------------------------------------------------------------------
+    # Toggle message commands
+    # --------------------------------------------------------------------------------
+    
     @commands.command(
         name='stop',
         help=
@@ -464,12 +435,10 @@ class Messages(commands.Cog):
         if stopenabled == True:
             if ctx.message.author.id == 607583934527569920 or ctx.message.author.id == 509239077212782592 or ctx.message.author.guild_permissions.administrator:
                 if command == 'kaczynskifull':
-                    global kaczynskifullstop
-                    kaczynskifullstop = True
+                    self.kaczynski_full_stop = True
                     await ctx.send("c.kaczynskifull stopped")
                 elif command == 'kaczynskifulldm':
-                    global kaczynskifulldmstop
-                    kaczynskifulldmstop = True
+                    self.kaczynski_full_dm_stop = True
                     await ctx.send("c.kaczynskidmfull stopped")
                 elif command == 'quranfullmsg':
                     global quranfullmsgstop
@@ -480,8 +449,7 @@ class Messages(commands.Cog):
                     quranfulldmstop = True
                     await ctx.send("c.quranfulldm stopped")
                 elif command == 'count':
-                    global countstop
-                    countstop = True
+                    self.count_stop = True
                     await ctx.send("c.count stopped")
                 else:
                     await ctx.send(
